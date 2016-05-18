ce4894ac9f0f3604c1a494fc8dc3761d1e045b2d
Evan Oman <evan058@gmail.com>
2016-05-18 16:56:28 -0500

    Cleaned up a bit more :coffee:

--- eliza.pl
+++ eliza.pl
@@ -18,16 +18,13 @@
 	he => "he",
 	am => "are",
 	are => "am",
-	"." => ""#
-	Gets rid of annoying punctuation
+	"." => ""		#Gets rid of annoying punctuation
 );
 
 #Used to store info about the user
 % info = (
-	name => "Student", #
-	default 'name'
-	major => "College"#
-	default 'major'
+	name => "Student",		#default 'name'
+	major => "College"		#default 'major'
 );
 
 #Looping variables
@@ -53,7 +50,7 @@ print ">Hello, I am your academic advisor Eliza. What is your name?\n";
 while ($cond)
 {
 	my $input = getInput();
-	if ($input!~/^([Ee]xit)$/)
+	if (($input !~ /^([Ee]xit)$/)
 	{
 		#Need to get name / major first
 		if (!@outputArray)
@@ -61,62 +58,49 @@ while ($cond)
 			getNameMajor($input);
 		}
 		#Look for question words
-		elsif($input = ~/\b(Who|What|Where|When|Why|How)\b/i)
 		{
 			handleQuestion($input);
-			file: ///C:/users/evan~1.oma/appdata/local/temp/tmpye4u0r.html 1/4
-				9 / 21 / 2014 C: \cygwin\ home\ evan.oman\ eliza.pl
 		}
 		#negative case
-		elsif($input = ~/(do(n't| not) think|I won't|can't|don't want to)/)
+		elsif ($input =~ /(do(n't| not) think|I won't|can't|don't want to)/)
 		{
 			printMessage(">You sound discouraged. ".$info {"major"}." is hard work but I think you can do it!\n ");
 		}
-			#Look for uncertainty
-		elsif($input = ~/^(I('m| am) not sure|I do(n't| know))/)
+		#Look for uncertainty
+		elsif ($input =~ /^(I('m| am) not sure|I do(n't| know))/)
 		{
 			printMessage(">".$info {"name"}.", I am concerned that you do not know much about your graduation plan.If you want to succeed at ".$info{"major "}."you must pay attention to the details.\n");
 		}
-				#Look for 'ask you a question' response
-		elsif($input = ~/((ask you a| I have a) (\b(new|different|unrelated)\b\s)?question|ask you something(new | different | unrelated) ? ) / )
+		#Look for 'ask you a question' response
+		elsif ($input =~ /((ask you a| I have a) (\b(new|different|unrelated)\b\s)?question|ask you something (new|different|unrelated)?)/)
 		{
 			printMessage($askQuestionResponse);
 		}
-			#look for emotions
-		elsif($input = ~/\b(overwhelmed|sad|emotional|angry|furious|hate|frustrated)\b/)
+		#look for emotions
+		elsif ($input =~ /^\b(I|You|HeWha|She)\b \b(\w+)\b (\b(my|me)\b \b(\w+)(\.)?)?/i)
 		{
 			printMessage(">No need to get emotional, we will figure this out!\n");
 		}
 		# The I(blank) case with the question response
-		elsif($input = ~/^\b(I|You|HeWha|She)\b \b(\w+)\b (\b(my|me)\b \b(\w+)(\.)?)?/i)
+		elsif($input = ~/^\b(I|You|He|She)\b \b(\w+)\b (\b(my|me)\b \b(\w+)(\.)?)?/i)
 		{
-			#
-			The start of our responses
+			#The start of our responses
 			$starter = "Why do you say ";
-			#
-			Performs the word mapping on our string
+			#Performs the word mapping on our string
 			$input = applyReflMapping($input);
-			#
-			Prints our response
+			#Prints our response
 			printMessage(">".$starter.$input."?\n");
 		}
 		else
 		{
 			printMessage($tellMeMore);
 		}
-	} 
+	}
 	else
 	{
 		my $goodbye = ">Good bye";
-		$goodbye = $goodbye.
-		", ".$info {
-			"name"
-		}.
-		", I hope you do well in ".$info {
-			"major"
-		};
-		printMessage($goodbye.
-			".\n");
+		$goodbye = $goodbye.", ".$info {"name"}.", I hope you do well in ".$info {"major"};
+		printMessage($goodbye.".\n");
 		$cond = 0;
 	}
 }
@@ -127,8 +111,8 @@ sub getInput
 	print "<";
 	my $input = < STDIN > ;
 	chomp($input);
-	#
-	Add to list of inputs
+
+	#Add to list of inputs
 	push(@inputArray, $input);
 	return $input;
 }
@@ -139,13 +123,13 @@ sub printMessage
 	my $message = $_[0];
 	push(@outputArray, $message);
 	my $arrLength = @outputArray;
-	#Don 't want a loop, restart the conversation if I have said the same thing more than twice in a
-	row
-	if ($arrLength >= 3 && $outputArray[-1] eq $outputArray[-2] && $outputArray[-1] eq
-		$outputArray[-3])
+
+	#Don't want a loop, restart the conversation if I have said the same thing more than twice in a row
+	if ($arrLength >= 3 && $outputArray[-1] eq $outputArray[-2] && $outputArray[-1] eq $outputArray[-3])
 	{
 		printMessage($changeSubject);
-	} else
+	}
+	else
 	{
 		print $message;
 	}
@@ -155,10 +139,7 @@ sub printMessage
 sub applyReflMapping 
 {
 	my $a = $_[0];
-	$a = ~s / \b(I | my | [Yy] ou | your | me | [Ss] he | [Hh] e | am | are | \.)\ b / $replace {
-		$1
-	}
-	/eg;
+	$a =~ s/\b(I|my|[Yy]ou|your|me|[Ss]he|[Hh]e|am|are|\.)\b/$replace{$1}/eg;
 	return $a;
 }
 
@@ -173,10 +154,10 @@ sub getNameMajor
 		{
 			$input = getInput();
 		}
-		if ($input!~/^([Ee]xit)$/)
+		if ($input !~ /^([Ee]xit)$/)
 		{
 			# Want to keep trying to get the name
-			if ($input = ~s / ^ ((Hello, ? | Hi, ? ) ? ? ([Mm] y name is | I am | [Cc] all me | I am | Im))?\b(\w + )\ b / $4 / eg)
+			if ($input =~ s/^((Hello,? |Hi,? )? ?([Mm]y name is |I am |[Cc]all me |I'm ))?\b(\w+)\b/$4/eg)
 			{
 				$info {"name"} = $input;
 				printMessage(">Nice to meet you ".$input.", what is your major?\n");
@@ -209,10 +190,11 @@ sub getMajor
 
 	while (!$haveMajor) {
 		my $input = getInput();
-		if ($input!~/^([Ee]xit)$/)
+		if ($input !~ /^([Ee]xit)$/)
 		{
 			#Want to keep trying to get the name
-			if ($input = ~s / ^ (([Mm] y major is | I am majoring in )) ? (\b(\w + )\ b) + /$4/eg) {
+			if ($input =~ s/^(([Mm]y major is |I am majoring in ))?(\b(\w+)\b)+/$4/eg)
+			{
 				$info {"major"} = $input;
 				printMessage(">I have always been interested in ".$input.", do you like it so far?\n");
 				$haveMajor = 1;
@@ -220,7 +202,6 @@ sub getMajor
 			else
 			{
 				printMessage($tryAgain);
-
 				#If this is true then we looped for too long
 				if ($outputArray[-1] ne $tryAgain)
 				{
@@ -237,7 +218,7 @@ sub getMajor
 	}
 }
 
-# handles statements in question form
+# Handles statements in question form
 sub handleQuestion
 {
 	#Every other question we will respond with "check google"
@@ -249,8 +230,8 @@ sub handleQuestion
 		$middle = "do you think";#
 		Need to clean out the 'do'
 		s
-		$input = ~s / \b[Dd] o\ b\ s //;
-		$input = ~s / \b([Ww] ho | [Ww] hat | [Ww] here | [Ww] hen | [Ww] hy | [Hh] ow much\ b(\w + ) | [Hh] ow many\ b(\w + ))\ b / $1 $middle / ;
+		$input =~ s/\b[Dd]o\b\s//;
+		$input =~ s/\b([Ww]ho|[Ww]hat|[Ww]here|[Ww]hen|[Ww]hy|[Hh]ow much \b(\w+)|[Hh]ow many\b(\w+))\b/$1 $middle/;
 		printMessage($input);
 	}
 	else
